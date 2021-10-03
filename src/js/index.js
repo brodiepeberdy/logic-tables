f3a8111242738564da1203b96441d53c0cfc5514
Brodie Peberdy <brodie.peberdy@gmail.com>
2021-10-03 16:48:24 +0100

    Revised algorithm, now correctly parses and evaluates expressions according to their placement of parentheses

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,65 @@
 // Triggered when the proposition is entered, handles solving of the proposition and generating the truth table.
 function submitProposition(form){
-  // Cleans the string of redundant whitespace, instantiates the Parser class for solving the proposition.
-  var proposition_input = document.getElementById("proposition");
-  var parser = new Parser(proposition_input.value);
-  proposition_input.value = parser.proposition;
-  parser.solver(parser.proposition);
-  var truth_results = parser.evaluator();
+  try {
+    // Cleans the string of redundant whitespace, instantiates the Parser class for solving the proposition.
+    var proposition_input = document.getElementById("proposition");
+    var parser = new Parser(proposition_input.value);
+    proposition_input.value = parser.proposition;
+    var truth_results = parser.solver(parser.proposition);
 
-  // Removes any existing truth tables. ID: "truth_table_results"
-  var el = document.getElementById("truth_table_results");
-  if (el) {
-    el.parentNode.removeChild(el);
-  }
-  // New table created.
-  var table = document.createElement('table');
-  table.id = "truth_table_results";
+    // Removes any existing truth tables. ID: "truth_table_results"
+    var el = document.getElementById("truth_table_results");
+    if (el) {
+      el.parentNode.removeChild(el);
+    }
+    var el = document.getElementById("warning");
+    if (el) {
+      el.parentNode.removeChild(el);
+    }
+    // New table created.
+    var table = document.createElement('table');
+    table.id = "truth_table_results";
 
-  // Isolates unique variables within the proposition. For example, ((P∧Q)∨(S∨P)) would return an array ["P", "Q", "S"].
-  var variables = [... new Set(parser.proposition.replace(/\W/g, ''))];
-  // Creates the first row of the truth table - this labels each variable's corresponding column, and finally the propostion being solved.
-  var tr, td, text;
-  var tr = document.createElement('tr');
-  for (var num = 0; num < variables.length; num++) {
-    td = document.createElement('td');
-    text = document.createTextNode(variables[num]);
-    td.appendChild(text);
-    tr.appendChild(td);
-  }
-  td = document.createElement('td');
-  text = document.createTextNode(parser.proposition);
-  td.appendChild(text); tr.appendChild(td); table.appendChild(tr);
-
-  // Iterates over each row, each row uniquely defined by the combination of truth values, and their corresponding proposition's truth value.
-  for (var row = 0; row < truth_results.length; row++){
-    tr = document.createElement('tr');
-    // Cycles through the dictionary at each element of the array representing a row.
+    // Isolates unique variables within the proposition. For example, ((P∧Q)∨(S∨P)) would return an array ["P", "Q", "S"].
+    var variables = [... new Set(parser.proposition.replace(/\W/g, ''))];
+    // Creates the first row of the truth table - this labels each variable's corresponding column, and finally the propostion being solved.
+    var tr, td, text;
+    var tr = document.createElement('tr');
     for (var num = 0; num < variables.length; num++) {
       td = document.createElement('td');
-      text = document.createTextNode(truth_results[row][variables[num]]);
-      td.appendChild(text); tr.appendChild(td);
+      text = document.createTextNode(variables[num]);
+      td.appendChild(text);
+      tr.appendChild(td);
     }
-    // Further, adds the proposition's resulting truth value at the end of the row.
     td = document.createElement('td');
-    text = document.createTextNode(truth_results[row]["result"]);
+    text = document.createTextNode(parser.proposition);
     td.appendChild(text); tr.appendChild(td); table.appendChild(tr);
+
+    // Iterates over each row, each row uniquely defined by the combination of truth values, and their corresponding proposition's truth value.
+    for (var row = 0; row < truth_results.length; row++){
+      tr = document.createElement('tr');
+      // Cycles through the dictionary at each element of the array representing a row.
+      for (var num = 0; num < variables.length; num++) {
+        td = document.createElement('td');
+        text = document.createTextNode(truth_results[row][variables[num]]);
+        td.appendChild(text); tr.appendChild(td);
+      }
+      // Further, adds the proposition's resulting truth value at the end of the row.
+      td = document.createElement('td');
+      text = document.createTextNode(truth_results[row]["result"]);
+      td.appendChild(text); tr.appendChild(td); table.appendChild(tr);
+    }
+    // Table appended to the HTML page.
+    var results_container = document.getElementById("results");
+    text = document.createElement('p');
+    text.id = "warning";
+    text.innerHTML = "Something look wrong? Make sure your expressions are contained within brackets!"
+    results_container.appendChild(text);
+    results_container.appendChild(table);
   }
-  // Table appended to the HTML page.
-  var results_container = document.getElementById("results");
-  results_container.appendChild(table);
+  catch(error) {
+    alert("Looks like something went wrong!")
+  }
 }
 
 // Appends the selected character to the end of the current proposition.
--- src/js/parser.js
+++ src/js/parser.js
@@ -1,86 +1,16 @@
 class Parser {
-  proposition; // Proposition entered by the user, cleaned of whitespace.
-  queue = [];
-
+  proposition; // Proposition entered by the user, cleaned of whitespace, adds brackets if needed.
   // Removes any whitespace from the proposition.
   constructor(proposition_input) {
     this.proposition = proposition_input.replaceAll(' ','');
   }
 
-  solver(expression, truth_values) {
-    // (A) => A
-    var containerOpening = expression.indexOf("(");
-    var containerEnding = expression.lastIndexOf(")");
-    var expression = expression.substring(1, expression.length-1);
-
-    var firstOpening = expression.indexOf("(");
-    var firstEnding = -1
-    var contained = 0
-    var first_found = false
-    while (firstEnding == -1 && first_found == false) {
-      for (var i = firstOpening + 1; i < expression.length; i++) {
-        if (expression[i] == "(") {
-          contained = contained + 1
-        }
-        else if (expression[i] == ")") {
-          if (contained == 0) {
-            firstEnding = i;
-            first_found = true;
-          }
-          else {
-            contained = contained - 1
-          }
-        }
-      }
-      break;
-    }
-
-    if (first_found == true) {
-      this.solver(expression.substring(firstOpening, firstEnding + 1))
-      var operator = expression[firstEnding + 1]
-      console.log(operator)
-      this.queue.push(operator);
-    }
-    else {
-      console.log(expression);
-      this.queue.push(expression);
-    }
-
-    var secondOpening = expression.substring(firstEnding + 2, expression.length).indexOf("(");
-    var secondEnding = -1;
-    contained = -1;
-    var second_found = false;
-    while (secondEnding == -1 && second_found == false) {
-      for (var i = secondOpening + 1 + firstEnding + 1; i < expression.length; i++) {
-        if (expression[i] == "(") {
-          contained = contained + 1;
-        }
-        else if (expression[i] == ")") {
-          if (contained == 0) {
-            secondEnding = i;
-            second_found = true;
-          }
-          else {
-            contained = contained - 1
-          }
-        }
-      }
-      break;
-    }
-
-    if (second_found == true) {
-      this.solver(expression.substring(secondOpening + 1 + firstEnding + 1, secondEnding + 1))
-    }
-  }
-
-  evaluator() {
-    const operators = ["∧", "∨", "→", "⟷"]
-
+  solver(expression) {
     // Generates all combinations of truth values for the variables.
-    var variables = [... new Set(this.proposition.replace(/\W/g, ''))];
-    var numberOfSets = 1 << variables.length;
+    var variables = [... new Set(expression.replace(/\W/g, ''))];
+    var set_count = 1 << variables.length;
     var truth_values = [];
-    for (var i = 0; i < numberOfSets; i++) {
+    for (var i = 0; i < set_count; i++) {
       truth_values.push({});
       for (var j = 0; j < variables.length; j++) {
         if (((1 << j) & i) > 0) {
@@ -90,122 +20,107 @@ class Parser {
           truth_values[i][variables[j]] = false;
         }
       }
+      truth_values[i]["result"] = null;
     }
 
-    var truth_results = [], elements;
-    var negate;
-    var temp_result, temp_op;
+    var sub_characters, sub_solution;
+    var final_solution = true;
+    var most_recent_opening = null, next_closing = null;
+    var characters, current_character, current_solutions;
 
-    for (var t = 0; t < truth_values.length; t++) {
-      var values = truth_values[t];
-      for (var q = 0; q < this.queue.length; q++) {
-        elements = this.queue[q].split(/([∧∨→⟷])/g).filter(Boolean);
-
-
-        temp_result = null;
-        temp_op = null;
-        for (var j = 0; j < elements.length; j++) {
-          negate = false;
-          if (elements[j].substring(0,1) === "¬") {
-            elements[j] = elements[j].substring(1,2);
-            negate = true;
+    // Iterates over every possible combination of truth values for the variables.
+    for (var t = 0; t < truth_values.length; t++){
+      current_solutions = truth_values[t];
+      // Proposition split into an array of characters, with each element being a single character.
+      // For example, '(A∨B)' translates to ['(', 'A', '∨', 'B', ')'].
+      characters = expression.split('');
+      // Continually evaluates each sub-formula of the proposition until the array has been whittled down to a single Boolean expression.
+      while (characters.length !== 1) {
+        // Iterates over each character in the array, looking for the "deepest" subformaula to be evaluated.
+        for (var i = 0; i < characters.length; i++) {
+          current_character = characters[i];
+          if (current_character === "(") {
+            most_recent_opening = i;
+            continue;
           }
-          if (variables.includes(elements[j])){
-            try {
-              var result = truth_values[t][elements[j]]
-              if (negate === true) {
-                result = !result;
-              }
-              if (temp_result == null) {
-                // If the proposition is an atomic single-variable expression, e.g. '(A)'.
-                if (variables.includes(elements[0]) && elements.length == 1) {
-                  truth_results.push(result)
-                }
-                temp_result = result;
-              }
-              else if (temp_op !== null) {
-                if (temp_op == "∧") {
-                  temp_result = temp_result && result;
-                }
-                else if (temp_op == "∨") {
-                  temp_result = (temp_result || result);
-                }
-                else if (temp_op == "→") {
-                  temp_result = (!temp_result || result);
-                }
-                else if (temp_op == "⟷") {
-                  temp_result = temp_result === result;
-                }
-                truth_results.push(temp_result);
-                temp_op = null;
-              }
-            }
-            catch(err) {
-              console.log("Looks like something went wrong!");
-            }
-          }
-          else if (operators.includes(elements[j])){
-            try {
-              if (elements.length == 1) {
-                truth_results.push(elements[j]);
-              }
-              else {
-                temp_op = elements[j];
-              }
-            }
-            catch(err) {
-              console.log("Looks like something went wrong!");
-            }
+          // Once an atomic proposition is identified, this proposition is evaluated.
+          else if (current_character === ")" && most_recent_opening !== null) {
+            next_closing = i;
+            sub_characters = characters.slice(most_recent_opening, next_closing + 1);
+            sub_solution = this.solve_sub(sub_characters, current_solutions);
+            // The evaluated expression is removed, and replaced with a single Boolean expression.
+            characters.splice(most_recent_opening, sub_characters.length);
+            characters.splice(most_recent_opening, 0, sub_solution);
+            // The brackets no longer exist, so the indexes need to be reset for the next subformula.
+            most_recent_opening = null;
+            next_closing = null;
+            continue;
           }
         }
       }
-      truth_results.push(" ")
-    }
-
-    var final = [];
-    var temp_result = null, temp_op = null;
-    for (var v = 0; v < truth_results.length; v++) {
-      if (truth_results[v] == true || truth_results[v] == false) {
-        if (truth_results[v] === " ") {
-          final.push(temp_result);
-          temp_result = null;
-          temp_op = null;
-        }
-        else if (temp_result == null) {
-          temp_result = truth_results[v];
-        }
-        else if (temp_op !== null) {
-          if (temp_op == "∧") {
-            temp_result = temp_result && truth_results[v];
-          }
-          else if (temp_op == "∨") {
-            temp_result = temp_result || truth_results[v];
-          }
-          else if (temp_op == "→") {
-            temp_result = (!temp_result || truth_results[v]);
-          }
-          else if (temp_op == "⟷") {
-            temp_result = temp_result === truth_results[v];
-          }
-          temp_op = null;
-        }
-      }
-      else if (operators.includes(truth_results[v])){
-        try {
-          temp_op = truth_results[v];
-        }
-        catch(err) {
-          console.log("Looks like something went wrong!");
-        }
-      }
-    }
-
-
-
-
-    for (var v = 0; v < truth_values.length; v++) {
-      truth_values[v]["result"] = final[v]
+      // Once the proposition has been evaluated to a single Boolean expression, this is appended to the dictionary representing the corresponding combination of truth values.
+      truth_values[t]["result"] = sub_solution;
     }
     return truth_values;
+  }
+
+  // Evaluates an expression in the form (...) and returns a single Boolean expression.
+  solve_sub(expression, truth_values) {
+    // Removes opening and closing brackets, find all unique variables.
+    var bracketless = expression.slice(1, -1);
+    var variables = [... new Set(this.proposition.replace(/\W/g, ''))];
+    const operators = ["∧", "∨", "→", "⟷"]
+    var temp_op= null;
+    var negate = false, result = null, variable_value;
+    // Iterates over each character in the expression, brackets removed.
+    for (var i = 0; i < bracketless.length; i++) {
+      // If it's a negation, make note of this and continue to next character.
+      if (bracketless[i] === "¬") {
+        negate = true;
+      }
+      // If it's an operator, make note of this and continue to next character.
+      else if (operators.includes(bracketless[i])) {
+        temp_op = bracketless[i];
+      }
+      // If it's a variable or a Boolean value, then continue.
+      else if (variables.includes(bracketless[i]) || bracketless[i] === true || bracketless[i] === false) {
+        // Sets current term to whatever the variable or truth value represents,
+        if (bracketless[i] === true || bracketless[i] === false) {
+          variable_value = bracketless[i];
+        }
+        else {
+          variable_value = truth_values[bracketless[i]];
+        }
+        // Negates the character if applicable.
+        if (negate === true) {
+          variable_value = !variable_value;
+          negate = false;
+        }
+        // If the expression is still null, then set it as the first truth value in the expression.
+        if (result === null) {
+          result = variable_value;
+        }
+        // If it's not the first variable / Boolean value, and the last character was an operator, then perform the relevant Boolean operation.
+        else if (temp_op !== null)
+        {
+          if (temp_op == "∧") {
+            result = result && variable_value;
+          }
+          else if (temp_op == "∨") {
+            result = (result || variable_value);
+          }
+          else if (temp_op == "→") {
+            result = (!result || variable_value);
+          }
+          else if (temp_op == "⟷") {
+            result = result === variable_value;
+          }
+          // Operation taken care of now.
+          temp_op = null;
+        }
+      }
+    }
+    // Return a single Boolean value.
+    return result;
   }
 }
